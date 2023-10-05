c90ef34ea28d5bc4e399101536573526a9b3d118
Lorenzo <lcano@unizar.es>
2023-10-05 12:38:31 +0200

    changed name, advanced development

--- lidar_to_other/src/nodes/pointcloud_to_voxelgrid.cpp
+++ lidar_to_other/src/nodes/pointcloud_to_voxelgrid.cpp
@@ -1,9 +1,15 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <geometry_msgs/Point.h>
 #include <std_msgs/String.h>
 #include <string>
 #include <math.h>
+
+struct Coordinates
+{
+    float xyz[3];
+};
 
 class LidarToVoxelGridNode
 {
@@ -14,8 +20,9 @@ private:
     std::string input_topic, output_topic;
     float voxel_size;
     float max_x, max_y, max_z;
-    float x_offset, y_offset, z_offset;
-    int length, width,height;
+    int length, width, height, size;
+    int *voxel_grid;
+
 public:
     LidarToVoxelGridNode(void)
     {
@@ -29,6 +36,24 @@ public:
         length = std::floor(max_x * 2 / voxel_size);
         width = std::floor(max_y * 2 / voxel_size);
         height = std::floor(max_z * 2 / voxel_size);
+        size = length * width * height;
+        voxel_grid = new int[size];
+        lidar_subscriber = nh.subscribe<sensor_msgs::PointCloud>(input_topic, 10, ptcl_callback);
+    }
+    int coordinates_to_index(float x, float y, float z)
+    {
+        int i = std::floor((x + max_x)/voxel_size);
+        int j = std::floor((y + max_y)/voxel_size);
+        int k = std::floor((z + max_z)/voxel_size);
+        return i + j*length + k * (length*width);
+    }
+    void ptcl_callback(sensor_msgs::PointCloud ptcl_msg)
+    {
+        int n_point = ptcl_msg.points.size();
+        geometry_msgs::Point32 point;
+        for (int i = 0; i++;i<n_point){
+            point = ptcl_msg.points[i];
+        }
     }
 };
 
